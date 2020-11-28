6e70fb85725a664166c25b42b39d3715250d9f2a
Trillzdboy <chukwudubem7@gmail.com>
2020-11-28 03:18:31 +0100

    Added manifest and service workers for offline support

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,198 @@
-/*button.addEventListener('click', function(){
-    var request = new XMLHttpRequest()
-    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=f6e72d8d9402d6a2fe860d4fd4630eab')
-    request.onload = function(){
-        var data = JSON.parse(this.response)
+var country = document.getElementById('country');
+var sys = document.getElementById('sys');
+var temperature = document.getElementById('temperature');
+var pressure = document.getElementById('pressure');
+var speed = document.getElementById('speed');
+var humidity = document.getElementById('humidity');
+var direction = document.getElementById('direction');
+
+var button = document.getElementById('clicked');
+var input = document.getElementById('input');
+
+function k2c(k) {
+    return Math.round(k - 273.15);
+}
+
+function degreesToDirection(degrees) {
+    var range = 360/16;
+    var low = 360 - range/2;
+    var high = (low + range) % 360;
+    var angles = [ "N", "NNE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    for (i in angles) {
         
-        if (request.status >= 200 && request.status < 400){
-            console.log (data)
-        } else {
-            console.log(err)
+        if( degrees >= low && degrees < high) {
+            return angles[i];
         }
+        
+        low = (low + range) % 360;
+        high = (high + range) % 360;
     }
+    return "N";
+}
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition)
+} else {
+    alert('Cannot discover location automatically. Use the search option!')
+}
+
+function showPosition(position) {
+    updateByGeo(position.coords.latitude, position.coords.longitude)
+}
+
+function updateByGeo(lat, lon) {
+    var request = new XMLHttpRequest()
+    request.open('Get', 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=f6e72d8d9402d6a2fe860d4fd4630eab', true)
+    request.onload = function () {
+    var data = JSON.parse(this.response);
     
-    request.send()
-}) */
-
-var button = document.getElementById('button')
-var input = document.getElementById('input')
-
-var request = new XMLHttpRequest()
-request.open('Get', 'https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=f6e72d8d9402d6a2fe860d4fd4630eab', true)
-request.onload = function () {
-    var data = JSON.parse(this.response)
-    console.log(data)
+    if (request.status >= 200 && request.status <400) {
+        console.log(data);
+        country.innerHTML = data.name;
+        sys.innerHTML = data.sys.country;
+        temperature.innerHTML = k2c(data.main.temp);
+        pressure.innerHTML = data.main.pressure;
+        speed.innerHTML = data.wind.speed;
+        humidity.innerHTML = data.main.humidity;
+        direction.innerHTML = degreesToDirection(data.wind.deg);
+        
+    } else {
+        console.log('error');
+    }
     
     
 }
 
-request.send()
+request.send();
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var request = new XMLHttpRequest()
+    request.open('Get', 'https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=f6e72d8d9402d6a2fe860d4fd4630eab', true)
+    request.onload = function () {
+    var data = JSON.parse(this.response);
+    
+    if (request.status >= 200 && request.status <400) {
+        console.log(data);
+        country.innerHTML = data.name;
+        sys.innerHTML = data.sys.country;
+        temperature.innerHTML = k2c(data.main.temp);
+        pressure.innerHTML = data.main.pressure;
+        speed.innerHTML = data.wind.speed;
+        humidity.innerHTML = data.main.humidity;
+        direction.innerHTML = degreesToDirection(data.wind.deg);
+        
+    } else {
+        console.log('error');
+    }
+    
+    
+}
+
+request.send(); */
+
+
+/*button.addEventListener("click", weather());
+
+function weather() {
+    var request = new XMLHttpRequest()
+    request.open('Get', 'https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=f6e72d8d9402d6a2fe860d4fd4630eab', true)
+    request.onload = function () {
+    var data = JSON.parse(this.response);
+    
+    if (request.status >= 200 && request.status <400) {
+        console.log(data);
+        country.innerHTML = data.name;
+        sys.innerHTML = data.sys.country;
+        temperature.innerHTML = k2c(data.main.temp);
+        pressure.innerHTML = data.main.pressure;
+        speed.innerHTML = data.wind.speed;
+        humidity.innerHTML = data.main.humidity;
+        direction.innerHTML = degreesToDirection(data.wind.deg);
+        
+    } else {
+        console.log('error');
+    }
+    
+    
+}
+
+request.send();
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ServiceWorker Registration
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js')
+            .then(registration => {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(err => {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
+
+// Custom Download Button Prompt
+let deferredPrompt;
+const downloadButton = document.querySelector('.download-button');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    // Make the Download App button visible.
+    downloadButton.style.display = 'inline-block'; 
+});
+
+downloadButton.addEventListener('click', (e) => {
+    deferredPrompt.prompt(); // This will display the Add to Homescreen dialog.
+    deferredPrompt.userChoice
+        .then(choiceResult => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+})
